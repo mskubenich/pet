3aebb7b31262d48248f728f98095228f198a7ba0
Mykhaylo Skubenych <mykhaylo.skubenych@gmail.com>
2015-05-25 22:59:23 +0300

    Registration form.

--- app/assets/javascripts/application.coffee
+++ app/assets/javascripts/application.coffee
@@ -10,3 +10,13 @@
 #= require ./client_app/application.module.js
 #= require_tree ./client_app/factories
 #= require_tree .
+
+
+
+
+window.previewAvatar = (input, selector) ->
+  if input.files && input.files[0]
+    reader = new FileReader()
+    reader.onload = (e) ->
+      $(selector).css('background', 'transparent url(' + e.target.result + ')')
+    reader.readAsDataURL(input.files[0])
--- app/assets/javascripts/client_app/users/users.controller.js
+++ app/assets/javascripts/client_app/users/users.controller.js
@@ -4,43 +4,30 @@
 
     angular.module('petModeApp')
         .controller('UsersController', ['$scope', '$state', 'ngDialog', function ($scope, $state, ngDialog) {
-            $scope.dulo = 'users dulo';
+            $scope.userData = {};
 
-            //$scope.userData = {
-            //    first_name: '',
-            //    last_name: '',
-            //    ssn_capture: '',
-            //    phone_number: '',
-            //    adress: '',
-            //    date_of_birth: null
-            //};
-            //
-            //$scope.submitUserData = function () {
-            //    if($scope.userDataForm.$invalid){
-            //        return false;
-            //    }
-            //
-            //    $loading.start('setup');
-            //    isup.submitUserData($scope.userData).success(function(data, status, headers, config){
-            //        $state.go('setup.' + (data.setup_state.next_step));
-            //        $loading.finish('setup');
-            //    }).error(function (data, status, headers, config) {
-            //        $loading.finish('setup');
-            //        if(data.errors){
-            //            hbDialog.showAlert('Error', JSON.stringify(data.errors));
-            //        }
-            //        if (status == 403){
-            //            $state.go('setup.' + (data.setup_state.next_step));
-            //        }else{
-            //            $scope.errors = data.validation_errors;
-            //        }
-            //    });
-            //};
-            //
-            //$scope.stepData = {};
-            //stepDataService.getStepData(function(data){
-            //    $scope.userData = data.step_data;
-            //});
+            $scope.submitUserData = function () {
+                //if($scope.userDataForm.$invalid){
+                //    return false;
+                //}
+                //
+                //$loading.start('setup');
+                //isup.submitUserData($scope.userData).success(function(data, status, headers, config){
+                //    $state.go('setup.' + (data.setup_state.next_step));
+                //    $loading.finish('setup');
+                //}).error(function (data, status, headers, config) {
+                //    $loading.finish('setup');
+                //    if(data.errors){
+                //        hbDialog.showAlert('Error', JSON.stringify(data.errors));
+                //    }
+                //    if (status == 403){
+                //        $state.go('setup.' + (data.setup_state.next_step));
+                //    }else{
+                //        $scope.errors = data.validation_errors;
+                //    }
+                //});
+
+
+            };
         }])
-
 }());
