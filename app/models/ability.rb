883235ee60a03899329b9f9ef078a1037881092b
Mykhaylo Skubenych <mykhaylo.skubenych@gmail.com>
2015-05-27 12:26:59 +0300

    Setup role abilities.

---
+++ app/models/ability.rb
@@ -0,0 +1,24 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    if user
+      if user.admin?
+        can :manage, :all
+      end
+      if user.user?
+        can :manage, User do |entity|
+          user.id == entity.id
+        end
+        can :read, Role
+        can :manage, Session do |session|
+          session.user.id == user.id
+        end
+      end
+    else
+      can :manage, User do |entity|
+        entity.new_record?
+      end
+    end
+  end
+end
