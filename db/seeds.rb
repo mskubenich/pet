6b8803957574dcefbcfb547d492e88fd295987de
Mykhaylo Skubenych <mykhaylo.skubenych@gmail.com>
2015-05-27 12:31:05 +0300

    Seed admin user.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts "Create admin ..."
+admin = User.create first_name: 'admin', last_name: 'admin', email: 'admin@petmode.com', login: 'admin', password: 'secret', password_confirmation: 'secret'
+admin.roles << Role.admin
