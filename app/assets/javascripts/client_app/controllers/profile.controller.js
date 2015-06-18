2dd22ea77ac2e63627cb1c6577c4194f0a35485f
Mykhaylo Skubenych <mykhaylo.skubenych@gmail.com>
2015-06-18 20:35:12 +0300

    Finish profile design.

--- app/assets/javascripts/client_app/controllers/profile.controller.js
+++ app/assets/javascripts/client_app/controllers/profile.controller.js
@@ -27,5 +27,11 @@
                 $scope.phone = $scope.profile.phone;
             }
 
+            $scope.articles = [
+                {title: 'Lorem ipsum dolor sit amet', scorp: true, created_at: "около 23 часов назад в 18:19", author: 'dog_sale', body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
+                {title: 'Lorem ipsum dolor ',created_at: "около 23 часов назад в 18:19", author: 'dog_sale', body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
+                {title: 'Lorem ipsum dolor sit amet',created_at: "около 23 часов назад в 18:19", author: 'dog_sale', body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in.'}
+            ];
+
         }])
 }());
