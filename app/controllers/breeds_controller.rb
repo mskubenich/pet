693fc8798e2709c7c00dab2281960342e3bb66ce
Mykhaylo Skubenych <mykhaylo.skubenych@gmail.com>
2015-07-09 02:22:22 +0300

    Fix ability to read Breeds.

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -1,5 +1,6 @@
 class BreedsController < ApplicationController
   load_and_authorize_resource :breed
+  skip_before_filter :authenticate_user
   def index
     query = Breed.all
     query = query.where family: params[:family] if params[:family] == 'cat' || params[:family] == 'dog'
