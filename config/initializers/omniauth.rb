873c46bac4a2a703b017cb9a2134635c7a90e6cb
Mykhaylo Skubenych <mykhaylo.skubenych@gmail.com>
2015-07-21 19:28:20 +0300

    Update facebook credentials.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :facebook, '728398983937918', '5bfbd9469bd843b820adf164f63f576b' #localhost
-  provider :facebook, '728398983937918', '5bfbd9469bd843b820adf164f63f576b' #staging
+  provider :facebook, '1748678858692615', '5a14574755204ba66a5bad2825307e50' #staging
 end
