2b769f9b5dc6fc9a0780b4c1b9d4089ff3eb3378
Mykhaylo Skubenych <mykhaylo.skubenych@gmail.com>
2015-07-22 14:08:39 +0300

    Login via odnoklassniki.

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ gem "selectize-rails"
 gem 'omniauth'
 gem 'omniauth-facebook'
 gem 'omniauth-vkontakte'
+gem 'omniauth-odnoklassniki'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,4 +3,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '1748678858692615', '5a14574755204ba66a5bad2825307e50' #staging
   # provider :vkontakte, '5004595', 'WEId2hiHYnug8t6FDIsN' #localhost
   provider :vkontakte, '5004777', 'X8FCUos9VhOSJCXlXrL8' #staging
+  provider :odnoklassniki, '1147009024', '8E70F282B395F1653B0EFE90', :public_key => 'CBAJPOFFEBABABABA' #localhost
 end
