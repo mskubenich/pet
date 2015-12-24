384921bc624ae5635bc1e0ecb38004dd76a4871b
Mykhaylo Skubenych <mykhaylo.skubenych@gmail.com>
2015-12-24 23:55:37 +0200

    Populate breeds.

--- app/assets/javascripts/client_app/controllers/admin/breeds.controller.js
+++ app/assets/javascripts/client_app/controllers/admin/breeds.controller.js
@@ -71,7 +71,7 @@
                             visiblePages: 9,
                             onPageClick: function (event, page) {
                                 $scope.page = page;
-                                $scope.retrieveCategories();
+                                $scope.retrieveBreeds();
                             }
                         });
                     }).error(function (data) {
--- app/models/breed.rb
+++ app/models/breed.rb
@@ -1,2 +1,4 @@
 class Breed < ActiveRecord::Base
+  validates :title_ru, presence: true, uniqueness: true
+  validates :title_en, presence: true, uniqueness: true
 end
