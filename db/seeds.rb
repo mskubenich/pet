2db57051b3226b3851e81fd91de91af1f6b07ff2
Mykhaylo Skubenych <mykhaylo.skubenych@gmail.com>
2015-12-29 13:17:04 +0200

    Fix production.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 puts "Create admin ..."
-admin = User.create first_name: 'admin', last_name: 'admin', email: 'admin@petmode.com', login: 'admin', password: 'secret', password_confirmation: 'secret', country: 'Country', city: 'City'
+admin = User.create first_name: 'admin', last_name: 'admin', email: 'admin@petmode.com', login: 'admin', password: 'secret', password_confirmation: 'secret', country_id: 1, city: 'City'
 admin.roles << Role.admin
 
 puts "Create users ..."
 50.times do |i|
-  user = User.create first_name: "user_#{ i }", last_name: "user_#{ i }", email: "user_#{ i }@petmode.com", login: "user_#{ i }", password: 'secret', password_confirmation: 'secret', country: 'Country', city: 'City'
+  user = User.create first_name: "user_#{ i }", last_name: "user_#{ i }", email: "user_#{ i }@petmode.com", login: "user_#{ i }", password: 'secret', password_confirmation: 'secret', country_id: 1, city: 'City'
   user.roles << Role.user
 end
 
