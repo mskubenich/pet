803f7ef121588d83363bbeb4a7f30f63b609ba7c
Mykhaylo Skubenych <mykhaylo.skubenych@gmail.com>
2016-01-26 16:16:47 +0200

    Fix Admin Users UI.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
+User.destroy_all
+Breed.destroy_all
+
 puts "Create admin ..."
 admin = User.create first_name: 'admin', last_name: 'admin', email: 'admin@petmode.com', login: 'admin', password: 'secret', password_confirmation: 'secret', country_id: 1, city: 'City'
 admin.roles << Role.admin
 
 puts "Create users ..."
-50.times do |i|
+14.times do |i|
   user = User.create first_name: "user_#{ i }", last_name: "user_#{ i }", email: "user_#{ i }@petmode.com", login: "user_#{ i }", password: 'secret', password_confirmation: 'secret', country_id: 1, city: 'City'
   user.roles << Role.user
 end
