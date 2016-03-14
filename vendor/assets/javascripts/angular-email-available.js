18fd53806ccc8c2740430e358a5f75e1ba1eefe9
Mykhaylo Skubenych <mykhaylo.skubenych@gmail.com>
2016-03-15 00:19:58 +0200

    fix

--- vendor/assets/javascripts/angular-email-available.js
+++ vendor/assets/javascripts/angular-email-available.js
@@ -1,54 +1,54 @@
-//'use strict';
-//
-//angular.module('validation.email', []);
-//
-//angular.module('validation.email').directive('emailvalidate', match);
-//
-//function match ($http) {
-//    return {
-//        require: 'ngModel',
-//        restrict: 'A',
-//        scope: {
-//            except: '=except'
-//        },
-//        link: function(scope, elem, attrs, ctrl) {
-//            if(!ctrl) {
-//                if(console && console.warn){
-//                    console.warn('Match validation requires ngModel to be on the element');
-//                }
-//                return;
-//            }
-//
-//            var validator = function(viewValue) {
-//                if (attrs.required && viewValue == ''){
-//                    return undefined;
-//                }
-//                if (viewValue && viewValue.match(/[a-zA-Z0-9\-_]+@[a-zA-Z0-9\-_]+\.[a-zA-Z0-9\-_]{2,}/)) {
-//                    ctrl.$setValidity('emailValid', true);
-//                    $http.post('/users/email_available', {email: viewValue, except: scope.except})
-//                        .success(function (data, status, headers, config) {
-//                            ctrl.$setValidity('emailAvailable', true);
-//                            return viewValue;
-//                        })
-//                        .error(function (data, status, headers, config) {
-//                            ctrl.$setValidity('emailAvailable', false);
-//                            return undefined;
-//                        });
-//                    return viewValue;
-//                } else {
-//                    ctrl.$setValidity('emailValid', false);
-//                    return undefined;
-//                }
-//
-//                return undefined;
-//            };
-//
-//
-//            scope.$watch(attrs.ngModel, function(value) {
-//                console.log('ss')
-//                validator(value);
-//            });
-//            //ctrl.$validators.validateEmail = validator;
-//        }
-//    };
-//}
+'use strict';
+
+angular.module('validation.email', []);
+
+angular.module('validation.email').directive('emailvalidate', match);
+
+function match ($http) {
+    return {
+        require: 'ngModel',
+        restrict: 'A',
+        scope: {
+            except: '=except'
+        },
+        link: function(scope, elem, attrs, ctrl) {
+            //if(!ctrl) {
+            //    if(console && console.warn){
+            //        console.warn('Match validation requires ngModel to be on the element');
+            //    }
+            //    return;
+            //}
+            //
+            //var validator = function(viewValue) {
+            //    if (attrs.required && viewValue == ''){
+            //        return undefined;
+            //    }
+            //    if (viewValue && viewValue.match(/[a-zA-Z0-9\-_]+@[a-zA-Z0-9\-_]+\.[a-zA-Z0-9\-_]{2,}/)) {
+            //        ctrl.$setValidity('emailValid', true);
+            //        $http.post('/users/email_available', {email: viewValue, except: scope.except})
+            //            .success(function (data, status, headers, config) {
+            //                ctrl.$setValidity('emailAvailable', true);
+            //                return viewValue;
+            //            })
+            //            .error(function (data, status, headers, config) {
+            //                ctrl.$setValidity('emailAvailable', false);
+            //                return undefined;
+            //            });
+            //        return viewValue;
+            //    } else {
+            //        ctrl.$setValidity('emailValid', false);
+            //        return undefined;
+            //    }
+            //
+            //    return undefined;
+            //};
+            //
+            //
+            //scope.$watch(attrs.ngModel, function(value) {
+            //    console.log('ss')
+            //    validator(value);
+            //});
+            //ctrl.$validators.validateEmail = validator;
+        }
+    };
+}
